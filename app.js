f6b9266ca38891243dee4bcd287b7fa641e56dfe
Juan Cruz <jc23729@gmail.com>
2021-03-12 16:00:23 -0500

    update

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const request = require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=b7660020ffeb0dd6c15dcbc412048749&query=37.8267,-122.4233'
 
-request({ url: url}, (error, response)=> {
+request({ url: url, json: true}, (error, response)=> {
     // console.log(response)  then run node app.js  in terminal will give you all the data 
     //we parse our data with JSON.parse and it lives on response.body
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+    // const data = JSON.parse(response.body)
+    // console.log(data.current)
+    //console.console.log(response.body.current)is doing the same as on top
+    console.log(response.body.current)
 })
 
