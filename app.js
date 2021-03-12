4a663e237fd42c086b979e0d1fb0c99f70da46c6
Juan Cruz <jc23729@gmail.com>
2021-03-12 15:43:20 -0500

    update

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const request = require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=b7660020ffeb0dd6c15dcbc412048749&query=37.8267,-122.4233'
 
-request({ url: url, json: true }, (error, response) => {
-    console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
+request({ url: url}, (error, response)=> {
+    // console.log(response)  then run node app.js  in terminal will give you all the data 
+    //we parse our data with JSON.parse and it lives on response.body
+    const data = JSON.parse(response.body)
 })
-
 
