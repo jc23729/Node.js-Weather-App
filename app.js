9dea55d069935b565ba5fc9194e77e7155c7d8f8
Juan Cruz <jc23729@gmail.com>
2021-03-12 15:27:07 -0500

    update

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const request = require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=b7660020ffeb0dd6c15dcbc412048749&query=37.8267,-122.4233'
 
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
+})
+
+
