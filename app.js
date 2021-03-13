553c12f2fce6606d2c85e0443c8fa4edfe8dcaee
Juan Cruz <jc23729@gmail.com>
2021-03-13 14:29:49 -0500

    update

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const request = require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=b7660020ffeb0dd6c15dcbc412048749&query=37.8267,-122.4233'
+// response.body.current
+//Goal: Print a small forecast to the user
+//Print : "It is currently 58.55 degrees out. There is a chance of 0% chance of rain"
+//Test your work
 
 request({ url: url, json: true}, (error, response)=> {
     // console.log(response)  then run node app.js  in terminal will give you all the data 
@@ -10,8 +14,4 @@ request({ url: url, json: true}, (error, response)=> {
     //console.console.log( m,mmmresponse.body.current)is doing the same as on top
     console.log("It is currently " + response.body.current.temperature + " degrees out. There is a " + response.body.current.precip + "Chance of rain")
 })
-// response.body.current
-//Goal: Print a small forecast to the user
-//Print : "It is currently 58.55 degrees out. There is a chance of 0% chance of rain"
-//Test your work
 
