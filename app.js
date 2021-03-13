b863c810526b4c6b2d6ca01fac85be131ab52208
Juan Cruz <jc23729@gmail.com>
2021-03-13 12:48:06 -0500

    update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ request({ url: url, json: true}, (error, response)=> {
     // const data = JSON.parse(response.body)
     // console.log(data.current)
     //console.console.log( m,mmmresponse.body.current)is doing the same as on top
-    console.log(response.body.current.weather_descriptions[0] + "It is currently" + response.body.current.temperature + "degrees out")
+    console.log("It is currently " + response.body.current.temperature + " degrees out. There is a " + response.body.current.precip + "Chance of rain")
 })
 // response.body.current
 //Goal: Print a small forecast to the user
