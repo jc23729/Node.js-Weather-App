ce21f79e309176eec500db9df0b5df481f2980f6
Juan Cruz <jc23729@gmail.com>
2021-03-13 16:40:01 -0500

    update

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const request = require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=b7660020ffeb0dd6c15dcbc412048749&query=37.8267,-122.4233&units=f'
+
+request({ url: url, json: true}, (error, response)=> {
+    console.log(error)
+})
+
+
+
 // response.body.current
 //Goal: Print a small forecast to the user
 //Print : "It is currently 58.55 degrees out. There is a chance of 0% chance of rain"
 //Test your work
 
-request({ url: url, json: true}, (error, response)=> {
-    // console.log(response)  then run node app.js  in terminal will give you all the data 
+//Previous code-------->
+// console.log(response)  then run node app.js  in terminal will give you all the data 
     //we parse our data with JSON.parse and it lives on response.body
     // const data = JSON.parse(response.body)
     // console.log(data.current)
     //console.console.log( m,mmmresponse.body.current)is doing the same as on top
-    console.log(response.body.current.weather_descriptions[0] + "It is currently " + response.body.current.temperature + " degrees out. There is a " + response.body.current.precip + "Chance of rain")
-})
+// request({ url: url, json: true}, (error, response)=> {
+//     console.log(response.body.current.weather_descriptions[0] + "It is currently " + response.body.current.temperature + " degrees out. There is a " + response.body.current.precip + "Chance of rain")
+// })
 
 //GeoCoding Service 
 // Address => Lat/Long  => Weather
